229af508ba4e2a7e7a9c74624e69886c74013e1b
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2025-04-03 23:15:28 -0300

    Add files via upload

---
+++ saude-beleza/js/script.js
@@ -0,0 +1,5 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal(".grid-conteudo", { duration: 3000 });
+sr.reveal(".video", { duration: 6000 });
+sr.reveal(".grid-equipe", { rotate: { x: 0, y: 100, z: 0 }, duration: 6000 });
